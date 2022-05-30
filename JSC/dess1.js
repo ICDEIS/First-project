3d8f8e41d4cfef5c97f33a2126f51f8ba9f0eb1e
IC Deis <deviss@LaFerta.AC800S>
2022-05-30 21:01:11 +0500

    new element added to JS file

--- JSC/dess1.js
+++ JSC/dess1.js
@@ -1,3 +1,5 @@
 let dess = {name: 'DEVISS', religion: 'Islam', age: 25}
 let deis = {name: 'ICDEIS', religion: 'Islam', age: 25}
 let edd = {name: 'EDDESS', religion: 'Islam', age: 25}
+
+let number = 23
