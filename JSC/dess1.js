8a22cdcd18c1169b4191dfe59ff457be0c8c4632
ICDEIS <rennarion@gmail.com>
2022-05-30 21:08:36 +0500

    fixing

--- JSC/dess1.js
+++ JSC/dess1.js
@@ -1,11 +1,7 @@
-let dess = {name: 'DEVISS', religion: 'Islam', age: 25}
-let deis = {name: 'ICDEIS', religion: 'Islam', age: 25}
-let edd = {name: 'EDDESS', religion: 'Islam', age: 25}
-<<<<<<< HEAD
-
-let number = 23
-=======
-const f1(user) {
+let dess = {name: 'DEVISS', religion: 'Islam', age: 25};
+let deis = {name: 'ICDEIS', religion: 'Islam', age: 25};
+let edd = {name: 'EDDESS', religion: 'Islam', age: 25};
+let number = 23;
+const f1 = (user) => {
   console.log(user.name) 
 }
->>>>>>> 91c77de93b6c5d846210ca056e8ebe80fcf71515
