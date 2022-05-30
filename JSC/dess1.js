91c77de93b6c5d846210ca056e8ebe80fcf71515
IC Deis <104900700+ICDEIS@users.noreply.github.com>
2022-05-30 19:17:13 +0500

    Update dess1.js
    new element added

--- JSC/dess1.js
+++ JSC/dess1.js
@@ -1,3 +1,6 @@
 let dess = {name: 'DEVISS', religion: 'Islam', age: 25}
 let deis = {name: 'ICDEIS', religion: 'Islam', age: 25}
 let edd = {name: 'EDDESS', religion: 'Islam', age: 25}
+const f1(user) {
+  console.log(user.name) 
+}
